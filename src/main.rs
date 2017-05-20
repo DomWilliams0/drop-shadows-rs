09a78d798db9afa1b2a9486b61ddb270fa2d57d7
Dom Williams <me@domwillia.ms>
2017-05-20 17:08:32 +0200

    Add experimental single edge shadow

--- src/main.rs
+++ src/main.rs
@@ -37,10 +37,30 @@ fn run() -> Result<(), image::ImageError> {
 
 fn mess_with_image(image: image::DynamicImage) -> Result<image::DynamicImage, image::ImageError> {
     const MARGIN: u32 = 40;
+    const BLACK_WIDTH: u32 = 4;
 
+    // create resized image
     let dims = image.dimensions();
     let mut resized = image::DynamicImage::new_rgba8(dims.0 + MARGIN * 2, dims.1 + MARGIN * 2);
     resized.copy_from(&image, MARGIN, MARGIN);
+
+    // add border
+    let black = image::Rgba::<u8> { data: [0, 0, 0, 255] };
+    for i in MARGIN..dims.0 + MARGIN {
+        for j in MARGIN - BLACK_WIDTH..MARGIN {
+            resized.put_pixel(i, j, black);
+        }
+    }
+
+    // blur top edge
+    {
+        let sub = resized
+            .sub_image(0, 0, dims.0 + MARGIN * 2, MARGIN)
+            .to_image();
+        let sub = image::imageops::blur(&sub, 7.0);
+        resized.copy_from(&sub, 0, 0);
+    }
+
 
     Ok(resized)
 }
