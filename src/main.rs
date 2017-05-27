b8465f5ea97125c30afce493a79c1922fbb114ee
Dom Williams <me@domwillia.ms>
2017-05-27 18:04:13 +0200

    Improve efficiency of shadow application

--- src/main.rs
+++ src/main.rs
@@ -37,25 +37,43 @@ fn run() -> Result<(), image::ImageError> {
 
 fn mess_with_image(image: image::DynamicImage) -> Result<image::DynamicImage, image::ImageError> {
     const MARGIN: u32 = 20;
+    const BLUR_MARGIN: u32 = 10;
 
     // create resized image
-    let dims = image.dimensions();
-    let mut resized = image::DynamicImage::new_rgba8(dims.0 + MARGIN * 2, dims.1 + MARGIN * 2);
+    let dims_orig = image.dimensions();
+    let mut resized = image::DynamicImage::new_rgba8(dims_orig.0 + MARGIN * 2,
+                                                     dims_orig.1 + MARGIN * 2);
+    let dims = resized.dimensions();
 
-    // fill with black
+    // create shadow
     let black = image::Rgba::<u8> { data: [0, 0, 0, 255] };
-    for y in MARGIN + 1..dims.1 + MARGIN {
-        for x in MARGIN + 1..dims.0 + MARGIN {
+    for y in MARGIN + 1..dims.1 - MARGIN {
+        for x in MARGIN + 1..dims.0 - MARGIN {
             resized.put_pixel(x, y, black);
         }
     }
 
-    // blur it
-    let mut resized = resized.blur(7.0);
+    // blur edges
+    // TODO ooer
+    let edges =
+        [((0, 0, dims.0, MARGIN + BLUR_MARGIN), (0, 0), (0, dims_orig.1 + BLUR_MARGIN)),
+         ((0, MARGIN + BLUR_MARGIN, MARGIN + BLUR_MARGIN, dims.1 - MARGIN * 2 - BLUR_MARGIN * 2),
+          (0, MARGIN + BLUR_MARGIN),
+          (dims_orig.0 + BLUR_MARGIN, 0))];
+
+    // apply blurred edges
+    for tup in &edges {
+        let ((x, y, w, h), (paste_x, paste_y), (rot_off_x, rot_off_y)) = *tup;
+        let view = resized.sub_image(x, y, w, h).to_image();
+        let view = image::imageops::blur(&view, 7.0);
+        resized.copy_from(&view, paste_x, paste_y);
+
+        let rot = image::imageops::rotate180(&view);
+        resized.copy_from(&rot, paste_x + rot_off_x, paste_y + rot_off_y);
+    }
 
     // copy original image across
     resized.copy_from(&image, MARGIN, MARGIN);
-
 
     Ok(resized)
 }
